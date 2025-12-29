aa4fa5325839b4863c87bb27f0942de093ceef40
Mahdi-500 <mahdi.tech1795@gmail.com>
2025-12-29 20:51:05 +0330

    changed the app name and temporarily disabled the general information updater

--- CarWebsite-venv/CarWebsite/Car/apps.py
+++ CarWebsite-venv/CarWebsite/Car/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
 
-class WebsiteConfig(AppConfig):
+class CarConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'
+    name = 'Car'
 
-    def ready(self):
-        import os
-        if os.environ.get("RUN_MAIN") == "true":
-            from .my_apps import schedular
-            schedular.start()
+    # def ready(self):
+    #     import os
+    #     if os.environ.get("RUN_MAIN") == "true":
+    #         from .my_apps import schedular
+    #         schedular.start()
