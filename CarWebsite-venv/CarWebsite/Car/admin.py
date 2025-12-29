a4267768c5da835ded85baaf627ed104dd078ecd
Mahdi-500 <mahdi.tech1795@gmail.com>
2025-12-29 20:49:43 +0330

    updated admin file

--- CarWebsite-venv/CarWebsite/Car/admin.py
+++ CarWebsite-venv/CarWebsite/Car/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-@admin.register(Cars)
-class CarsAdmin(admin.ModelAdmin):
+@admin.register(submittingNewCars)
+class submittingNewCarsAdmin(admin.ModelAdmin):
     list_display = ["manufacturer", "car_model"]
 
 @admin.register(GeneralInformation)
